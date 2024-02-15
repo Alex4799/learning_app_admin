3615559e05efcd7fe3d013245c513efcca896a14
Alex <mr.alex4799@gmail.com>
2024-02-15 11:02:12 +0630

    finish 1.2

--- resources/views/user/account/profile.blade.php
+++ resources/views/user/account/profile.blade.php
@@ -3,14 +3,56 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <link rel="shortcut icon" href="{{asset('image/code-solid.svg')}}" type="image/x-icon">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <style>
+        body{
+            background-image: linear-gradient(to right, #22c1c3 , #e579f3);
+        }
+        .button{
+            width: 100%;
+            height: 50px;
+            padding: 5px;
+            background-image: linear-gradient(to right, #22c1c3 , #e579f3);
+            border: none;
+            outline: none;
+            color: white;
+            font-weight: bolder;
+            border-radius: 10px;
+        }
+    </style>
+
+    <title>Angle</title>
 </head>
 <body>
-    User
-    <form action="{{route('logout')}}" method="post">
-        @csrf
-        <input type="submit" value="logout">
-    </form>
+    <div class="d-flex justify-content-center align-items-center" style="height: 100vh;">
+        <div class="container-fluid row">
+            <div class="col-lg-4 offset-lg-4 bg-white shadow p-3 rounded">
+                <div class="p-4">
+                    <h3 class="text-danger">You Don't have permission for this website. Please Logout.</h3>
+                    <form action="{{route('logout')}}" method="post" class="d-flex justify-content-center">
+                        @csrf
+                        <input type="submit" class="btn btn-danger" value="logout">
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4=" crossorigin="anonymous"></script>
+    <script>
+        $(document).ready(function(){
+            $.ajax({
+                type:'get',
+                url:'get/userInterface',
+                dataType:'json',
+                success:function(data){
+                    $('#title').html(data.title);
+                    $('#category').html(data.category);
+                }
+            })
+        })
+    </script>
+
 </body>
 </html>
